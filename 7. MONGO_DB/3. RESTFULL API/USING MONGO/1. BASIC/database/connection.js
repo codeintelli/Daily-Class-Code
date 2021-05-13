18f593aec379a718107ff1e3557c83c1f8f5a0bf
itvmh <itvmh2205@gmail.com>
2021-05-14 00:17:53 +0530

    CONNECTION CHANGES DUE TO GUIDELINES

--- 7. MONGO_DB/3. RESTFULL API/USING MONGO/1. BASIC/database/connection.js
+++ 7. MONGO_DB/3. RESTFULL API/USING MONGO/1. BASIC/database/connection.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
+// mongodb+srv://reactapp:reactapp2021@cluster0.6uatl.mongodb.net/codeintelli?retryWrites=true&w=majority
+/* IF YOU WANT TO USE THIS DATABASE THEN YOU CAN BUT I THINK YOU HAVE TO CREATE YOUR OWN ACCOUNT AND USE IN IT */
 mongoose
-  .connect(
-    `mongodb+srv://reactapp:reactapp2021@cluster0.6uatl.mongodb.net/codeintelli?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(`ENTER YOUR URL HERE`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log(`your database is successfully connected`);
   })
