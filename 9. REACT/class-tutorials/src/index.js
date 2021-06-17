85f3fe1f4592687a5238a7e7a16028d922f98ac0
itvmh <itvmh2205@gmail.com>
2021-06-17 18:00:20 +0530

    react day 3

--- 9. REACT/class-tutorials/src/index.js
+++ 9. REACT/class-tutorials/src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-let fname = "shiva";
-let lname = "mahadev";
+// external css
+import "./index.css";
+// let fname = "shiva";
+// let lname = "bholenath";
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let curDate = new Date().toLocaleDateString();
+let curTime = new Date().toLocaleTimeString();
+
+// internal css
+const pstyle = {
+  marginTop: "30px",
+  background: "green",
+  color: "white",
+};
 
 ReactDOM.render(
   <>
-    <h1>Shiva</h1>
-    <p>Mahadev</p>
-    <ol>{`my name is ${lname} ${fname}`}</ol>
-    <ol>{2 + 3}</ol>
+    <h1 className="heading">Shiva</h1>
+    <p style={pstyle}>{curDate}</p>
+    {/* inline css */}
+    <p style={{ fontSize: "50px" }}>{curTime}</p>
   </>,
   document.getElementById("root")
 );
